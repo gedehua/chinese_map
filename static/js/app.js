5790eae6666d55a9879ee04c35a64b1426c659d8
gedehua <1656995085@qq.com>
2022-04-14 09:40:12 +0800

    1

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,11 @@
 //地图容器
 var chart = echarts.init(document.getElementById('main'));
+
+window.onresize = function (event) {
+    chart.resize();
+    //console.log(event.type)
+};
+
 //34个省、市、自治区的名字拼音映射数组
 var provinces = {
     //23个省
@@ -43,145 +49,153 @@ var provinces = {
 };
 
 //直辖市和特别行政区-只有二级地图，没有三级地图
-var special = ["北京","天津","上海","重庆","香港","澳门"];
+var special = ["北京", "天津", "上海", "重庆", "香港", "澳门"];
 var mapdata = [];
 //绘制全国地图
-$.getJSON('static/map/china.json', function(data){
-	d = [];
-	for( var i=0;i<data.features.length;i++ ){
-		d.push({
-			name:data.features[i].properties.name
-		})
-	}
-	mapdata = d;
-	//注册地图
-	echarts.registerMap('china', data);
-	//绘制地图
-	renderMap('china',d);
+$.getJSON('static/map/china.json', function (data) {
+    d = [];
+    for (var i = 0; i < data.features.length; i++) {
+        d.push({
+            name: data.features[i].properties.name
+        })
+    }
+    console.log(d);
+    mapdata = d;
+    //注册地图
+    echarts.registerMap('china', data);
+    //绘制地图
+    renderMap('china', d);
 });
 
 //地图点击事件
 chart.on('click', function (params) {
-	console.log( params );
-	if( params.name in provinces ){
-		//如果点击的是34个省、市、自治区，绘制选中地区的二级地图
-		$.getJSON('static/map/province/'+ provinces[params.name] +'.json', function(data){
-			echarts.registerMap( params.name, data);
-			var d = [];
-			for( var i=0;i<data.features.length;i++ ){
-				d.push({
-					name:data.features[i].properties.name
-				})
-			}
-			renderMap(params.name,d);
-		});
-	}else if( params.seriesName in provinces ){
-		//如果是【直辖市/特别行政区】只有二级下钻
-		if(  special.indexOf( params.seriesName ) >=0  ){
-			renderMap('china',mapdata);
-		}else{
-			//显示县级地图
-			$.getJSON('static/map/city/'+ cityMap[params.name] +'.json', function(data){
-				echarts.registerMap( params.name, data);
-				var d = [];
-				for( var i=0;i<data.features.length;i++ ){
-					d.push({
-						name:data.features[i].properties.name
-					})
-				}
-				renderMap(params.name,d);
-			});	
-		}	
-	}else{
-		renderMap('china',mapdata);
-	}
+    console.log(params);
+    if (params.name in provinces) {
+        //如果点击的是34个省、市、自治区，绘制选中地区的二级地图
+        $.getJSON('static/map/province/' + provinces[params.name] + '.json', function (data) {
+            echarts.registerMap(params.name, data);
+            var d = [];
+            for (var i = 0; i < data.features.length; i++) {
+                d.push({
+                    name: data.features[i].properties.name
+                })
+            }
+            renderMap(params.name, d);
+        });
+    } else if (params.seriesName in provinces) {
+        //如果是【直辖市/特别行政区】只有二级下钻
+        if (special.indexOf(params.seriesName) >= 0) {
+            renderMap('china', mapdata);
+        } else {
+            //显示县级地图
+            $.getJSON('static/map/city/' + cityMap[params.name] + '.json', function (data) {
+                echarts.registerMap(params.name, data);
+                var d = [];
+                for (var i = 0; i < data.features.length; i++) {
+                    d.push({
+                        name: data.features[i].properties.name
+                    })
+                }
+                renderMap(params.name, d);
+            });
+        }
+    } else {
+        renderMap('china', mapdata);
+    }
 });
 
 //初始化绘制全国地图配置
 var option = {
-	backgroundColor: '#000',
-    title : {
-        text: 'Echarts3 中国地图下钻至县级',
-        subtext: '三级下钻',
-        link:'http://www.ldsun.com',
+    backgroundColor: '#fff',
+    title: {
+        text: '疫情物流信息',
+        subtext: 'china',
         left: 'center',
-        textStyle:{
-            color: '#fff',
-            fontSize:16,
-            fontWeight:'normal',
-            fontFamily:"Microsoft YaHei"
+        textStyle: {
+            color: '#000',
+            fontSize: 16,
+            fontWeight: 'normal',
+            fontFamily: "Microsoft YaHei"
         },
-        subtextStyle:{
-        	color: '#ccc',
-            fontSize:13,
-            fontWeight:'normal',
-            fontFamily:"Microsoft YaHei"
+        subtextStyle: {
+            color: '#6A6467',
+            fontSize: 13,
+            fontWeight: 'normal',
+            fontFamily: "Microsoft YaHei"
         }
     },
+    //echarts中tooltip的位置设置
     tooltip: {
         trigger: 'item',
-        formatter: '{b}'
+        //直接设置position的值（必须设置trigger: 'item'）
+        formatter: '{b}',
+        //position: 'top'
     },
     toolbox: {
+        //工具栏是否可见
         show: true,
+        //默认值为horizontal，工具栏 icon 的布局朝向。可选项为“horizontal”和“vertical”
         orient: 'vertical',
+        //水平位置
         left: 'right',
+        //竖直位置
         top: 'center',
         feature: {
-            dataView: {readOnly: false},
+            dataView: {
+                readOnly: false
+            },
             restore: {},
             saveAsImage: {}
         },
-        iconStyle:{
-        	normal:{
-        		color:'#fff'
-        	}
+        iconStyle: {
+            normal: {
+                color: '#F04F28'
+            }
         }
     },
-    animationDuration:1000,
-	animationEasing:'cubicOut',
-	animationDurationUpdate:1000
-     
+    animationDuration: 1000,
+    animationEasing: 'cubicOut',
+    animationDurationUpdate: 1000
+
 };
-function renderMap(map,data){
-	option.title.subtext = map;
-    option.series = [ 
-		{
-            name: map,
-            type: 'map',
-            mapType: map,
-            roam: false,
-            nameMap:{
-			    'china':'中国'
-			},
-            label: {
-	            normal:{
-					show:true,
-					textStyle:{
-						color:'#999',
-						fontSize:13
-					}  
-	            },
-	            emphasis: {
-	                show: true,
-	                textStyle:{
-						color:'#fff',
-						fontSize:13
-					}
-	            }
-	        },
-	        itemStyle: {
-	            normal: {
-	                areaColor: '#323c48',
-	                borderColor: 'dodgerblue'
-	            },
-	            emphasis: {
-	                areaColor: 'darkorange'
-	            }
-	        },
-            data:data
-        }	
-    ];
+
+function renderMap(map, data) {
+    option.title.subtext = map;
+    option.series = [{
+        name: map,
+        type: 'map',
+        mapType: map,
+        roam: false,
+        nameMap: {
+            'china': '中国'
+        },
+        label: {
+            normal: {
+                show: true,
+                textStyle: {
+                    color: '#999',
+                    fontSize: 13
+                }
+            },
+            emphasis: {
+                show: true,
+                textStyle: {
+                    color: '#fff',
+                    fontSize: 13
+                }
+            }
+        },
+        itemStyle: {
+            normal: {
+                areaColor: '#323c48',
+                borderColor: 'dodgerblue'
+            },
+            emphasis: {
+                areaColor: 'darkorange'
+            }
+        },
+        data: data
+    }];
     //渲染地图
     chart.setOption(option);
 }
